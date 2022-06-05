b662ed93046ce53cbca8d8d3031279a8ca3695a1
Santiago Salacodigo <santiago.salamanca7@gmail.com>
2022-06-05 17:09:54 -0500

    Form validation, printing results, before moment.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,21 @@
 //Constants
 const inputName = document.getElementById('name');
-const inputFirstWorkDay = document.getElementById('first-workday');
-const inputFinalWorkday = document.getElementById('final-workday');
+const inputNumberOfDays = document.getElementById('number-of-days')
 const inputMorningStartTime = document.getElementById('morning-start-time');
 const inputMorningArriveTime = document.getElementById('morning-arrive-time');
 const inputNigthStartTime = document.getElementById('nigth-start-time');
 const inputNigthArriveTime = document.getElementById('nigth-arrive-time');
 
-const submitButton = document.getElementById('')
+const submitButton = document.getElementById('submit-btn');
+const cleanButton = document.getElementById('clean-results-btn');
+
 
 const divResults = document.getElementById('results');
 
 //Object form
 let informationObject = {
     name: "",
-    startDay: "",
-    finalDay: "",
+    numberOfDays: "",
     morningStartTime: "",
     morningArriveTime: "",
     nigthStartTime: "",
@@ -32,8 +32,8 @@ function eventListeners(){
     document.addEventListener('DOMContentLoaded', () => {
         form.addEventListener('submit', submitForm)
         writeDefaultValuesInForm();
-
     })
+    cleanButton.addEventListener('click', cleanResults);
 
 }
 
@@ -42,9 +42,7 @@ function eventListeners(){
 
 function writeDefaultValuesInForm(){
     inputName.value = "Santiago";
-    inputFirstWorkDay.value = '2022-06-05';
-    inputFinalWorkday.value = '2022-06-06';
-    
+    inputNumberOfDays.value = 1;
     inputMorningStartTime.value = '08:00';
     inputMorningArriveTime.value = '09:30';
     inputNigthStartTime.value = '17:50';
@@ -54,21 +52,34 @@ function writeDefaultValuesInForm(){
 
 function submitForm(e){
     e.preventDefault();
+
+    cleanResults();
+
     let informationObject = {};
 
     const name = inputName.value;
-    const firstWorkDay = inputFirstWorkDay.value;
-    const fiinalWorkday  = inputFinalWorkday.value;
+    const numberOfDays = Number(inputNumberOfDays.value);
     const morningStartTime = inputMorningStartTime.value;
     const morningArriveTime = inputMorningArriveTime.value;
     const nigthStartTime = inputNigthStartTime.value;
     const nigthArriveTime = inputNigthArriveTime.value;
 
+    if(
+        name == "" ||
+        (isNaN(numberOfDays) || numberOfDays <= 0) ||
+        morningStartTime == "" ||
+        morningArriveTime == "" ||
+        nigthStartTime == "" ||
+        nigthArriveTime == ""
+
+    ) {
+        return;
+    }
+
 
     informationObject = {
         name,
-        firstWorkDay,
-        fiinalWorkday,
+        numberOfDays,
         morningStartTime,
         morningArriveTime,
         nigthStartTime,
@@ -77,10 +88,69 @@ function submitForm(e){
 
     console.log(informationObject)
 
-    calculateTimeDifference();
+    calculateTimeDifference(informationObject);
 }
 
-function calculateTimeDifference(){
-    console.log("Calculando...");
+//Calculating values
+function calculateTimeDifference(informationObject){
 
+    const {
+        morningStartTime,
+        morningArriveTime,
+        nigthStartTime,
+        nigthArriveTime
+    } = informationObject;
+    
+    let diferenciaEntreHoras = 0;
+    diferenciaEntreHoras = morningArriveTime - morningStartTime;
+
+    console.log({diferenciaEntreHoras});
+
+    let morningTime = '00:20:00';
+    let nigthTime = '00:40:00';
+
+    let timeDifferences = {
+        morningTime,
+        nigthTime
+    }
+    
+    printTimeDifferenceResults(timeDifferences);
+}
+
+// Showing results
+function printTimeDifferenceResults(timeDifferences){
+
+    const divTimeResults = document.getElementById('time-results');
+
+
+    for (time in timeDifferences){
+        const divResult = document.createElement('div');
+
+        const resultText = document.createElement('p');
+        resultText.innerHTML = `Tiempo de viaje: ${time}`;
+        
+        const resultValue = document.createElement('p');
+        resultValue.innerHTML = `${timeDifferences[time]}`;
+
+        divResult.appendChild(resultText);
+        divResult.appendChild(resultValue);
+
+        divTimeResults.appendChild(divResult);
+    }
+
+}
+
+function cleanResults(){
+    const divTimeResults = document.getElementById('time-results');
+    cleanHTML(divTimeResults);
+}
+
+function resetForm(){
+    form.reset();
+}
+
+function cleanHTML(container){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
 }
