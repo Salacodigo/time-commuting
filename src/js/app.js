86bf881f42e89b6939038039a3d515e6a438f5d8
Santiago Salacodigo <santiago.salamanca7@gmail.com>
2022-06-05 21:59:22 -0500

    calculus completed, complete logic, time formated

--- src/js/app.js
+++ src/js/app.js
@@ -86,51 +86,168 @@ function submitForm(e){
         nigthArriveTime
     }
 
-    console.log(informationObject)
-
     calculateTimeDifference(informationObject);
 }
 
 //Calculating values
 function calculateTimeDifference(informationObject){
-
     const {
+        numberOfDays,
         morningStartTime,
         morningArriveTime,
         nigthStartTime,
         nigthArriveTime
     } = informationObject;
+
+    let morningTime = '00:00';
+    let nigthTime = '00:00';
+
+    try {
+        morningTime = timeDifference(morningStartTime, morningArriveTime);
+        nigthTime = timeDifference(nigthStartTime, nigthArriveTime);
     
-    let diferenciaEntreHoras = 0;
-    diferenciaEntreHoras = morningArriveTime - morningStartTime;
+    } catch (error) {
+        console.error(error);
+        return;
+    }
 
-    console.log({diferenciaEntreHoras});
+    let totalTimePerDay = timeSum({morningTime,
+        nigthTime});
+    let totalTime = calculateTotalTime(totalTimePerDay, numberOfDays);
 
-    let morningTime = '00:20:00';
-    let nigthTime = '00:40:00';
-
-    let timeDifferences = {
+    let timesObj = {
         morningTime,
-        nigthTime
+        nigthTime,
+        totalTimePerDay,
+        totalTime
     }
     
-    printTimeDifferenceResults(timeDifferences);
+    printTimeDifferenceResults(timesObj);
+}
+
+function timeDifference(startTime, endTime){
+
+    let startTimeArray = startTime.split(':');
+    let endTimeArray = endTime.split(':');
+
+    let differenceCalculated =[]
+
+    if (Number(endTimeArray[1]) - Number(startTimeArray[1]) < 0){
+        differenceCalculated[0] = 
+            Number(endTimeArray[0]) - startTimeArray[0] - 1,
+        differenceCalculated[1] = 
+            Number(endTimeArray[1]) + 60 - Number(startTimeArray[1])
+    } else {
+        differenceCalculated = [
+            endTimeArray[0] - startTimeArray[0],
+            endTimeArray[1] - startTimeArray[1]
+        ]
+    }
+
+    let formatedTextTime = formatTime(differenceCalculated);
+
+    return formatedTextTime;
+}
+
+function timeSum(timeValues){
+    let result = [0,0];
+
+    for(time in timeValues){
+        console.log([time, timeValues[time]]);
+        let splitTime = timeValues[time].split(':');
+        result[0] += Number(splitTime[0]);
+        result[1] += Number(splitTime[1]);   
+    }
+
+    result[0] = result[0] + Math.floor(result[1] / 60);
+    result[1] = result[1] % 60;
+
+    let formatedTimeText = formatTime(result);
+
+    return formatedTimeText;
+}
+
+function calculateTotalTime(totalTimePerDay, numberOfDays){
+    
+    const minutesPerHour = 60;
+    const hoursPerDay = 24;
+    const daysPerMonth = 30;
+    const monthsPerYear = 12;
+
+    let timeSplit = totalTimePerDay.split(':');
+    let minutes = 4;
+    let hours = 3;
+    let days = 2;
+    let months = 1;
+    let years = 0;
+
+    let result = [0,0,0,0,0] // years, months, days, hours, minutes
+
+    //Total time
+    result[minutes] = timeSplit[1] * numberOfDays;
+    result[hours] = timeSplit[0] * numberOfDays;
+
+    console.log({timeSplit, result})
+
+    //Formating time
+    result[hours] = result[hours] + Math.floor(result[minutes] / minutesPerHour);
+    result[minutes] = result[minutes] % minutesPerHour;
+    
+    result[days] = result[days] + Math.floor(result[hours] / hoursPerDay);
+    result[hours] = result[hours] % hoursPerDay;
+
+    result[months] = result[months] + Math.floor(result[days] / daysPerMonth);
+    result[days] = result[days] % daysPerMonth;
+
+    result[years] = result[years] + Math.floor(result[months] / monthsPerYear);
+    result[months] = result[months] % monthsPerYear;
+    
+    console.log({result});
+    let formatedTextTime = formatTime(result);
+
+    return formatedTextTime;
+}
+
+function formatTime(array){
+    let resultText = '';
+    let arraySize = array.length;
+    console.log({arraySize})
+
+    for( let position = 0; position < arraySize ; position++ ){
+        if(position == arraySize -1){
+            if(array[position] < 10){
+                resultText += `0${array[position]}`;
+            } else {
+                resultText += `${array[position]}`;
+            }
+        } else {
+            if(array[position] < 10){
+                resultText += `0${array[position]}:`;
+            } else {
+                resultText += `${array[position]}:`;
+            }
+        }
+    }
+
+    return resultText;
 }
 
 // Showing results
-function printTimeDifferenceResults(timeDifferences){
+function printTimeDifferenceResults(timeResultsObj){
 
     const divTimeResults = document.getElementById('time-results');
 
 
-    for (time in timeDifferences){
+    for (time in timeResultsObj){
         const divResult = document.createElement('div');
 
         const resultText = document.createElement('p');
         resultText.innerHTML = `Tiempo de viaje: ${time}`;
         
         const resultValue = document.createElement('p');
-        resultValue.innerHTML = `${timeDifferences[time]}`;
+        resultValue.innerHTML = `${timeResultsObj[time]}`;
+
+        //formatTime
 
         divResult.appendChild(resultText);
         divResult.appendChild(resultValue);
