da3d46a9bc73af764b452f1eacecd502509e8ee6
Santiago Salacodigo <santiago.salamanca7@gmail.com>
2022-06-24 13:19:44 -0500

    Connect to Backend in Heroku

--- src/js/Services/formResponseService.js
+++ src/js/Services/formResponseService.js
@@ -1,4 +1,5 @@
-const url = "http://127.0.0.1:4000";
+// const url = "http://127.0.0.1:4000"; // Localhost
+const url = "https://time-commuting-backend.herokuapp.com/"; // Heroku
 
 async function postResponse(informationObject){
 
@@ -11,7 +12,7 @@ async function postResponse(informationObject){
         body: JSON.stringify(informationObject)
     });
     let response = await request.json();
-    
+
     console.log({"PostResponse":response});
 
  }
